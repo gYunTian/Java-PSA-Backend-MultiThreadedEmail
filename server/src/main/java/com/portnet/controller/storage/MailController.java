4f8901ce8602f9265069899fb5c96e375a2fb13a
Constance <hytan.2018@sis.smu.edu.sg>
2020-10-28 17:27:17 +0800

    import httpstatus to fix build error, minor edits to readme

--- server/src/main/java/com/portnet/controller/storage/MailController.java
+++ server/src/main/java/com/portnet/controller/storage/MailController.java
@@ -3,6 +3,7 @@ package com.portnet.controller.storage;
 import com.portnet.entity.storage.User;
 import com.portnet.service.storage.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
