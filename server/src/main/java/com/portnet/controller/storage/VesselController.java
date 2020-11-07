02a4e004e12225900d04c045ba1e631b35a1ff9e
Constance <hytan.2018@sis.smu.edu.sg>
2020-11-07 19:09:17 +0800

    fix CORS bug by adding back the dependency

--- server/src/main/java/com/portnet/controller/storage/VesselController.java
+++ server/src/main/java/com/portnet/controller/storage/VesselController.java
@@ -8,9 +8,7 @@ import com.portnet.service.storage.VesselService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * REST APIs using service methods for Vessel
